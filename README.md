be0d55cdd09651f077b0cbb04ddc3bf3e52ab3bd
Sebastian Rodriguez <me@srod.ca>
2023-11-01 18:31:28 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# uoft-ischool-course-navigator
-A Python script to scrape course data from the University of Toronto iSchool's website
+# University of Toronto iSchool Course Navigator
+This program helps users navigate the Univeristy of Toronto's Faculty of Information (iSchool)'s course catalogue. It scrapes data directly from the Faculty's website, and makes it available to users through a command-line interface. Users can search for an undergraduate or graduate course using the course's code (ex: `INF452`) and view information such as the course title, official course description, and any available syllabi from previous sessions. 
+
+## Getting Started
+
+### Requirements
+This script was developed in Python and requires at least Python 3 to run (tested using Python version 3.11.5). The program is also dependant on the [Requests](https://requests.readthedocs.io/en/latest/) HTTP client library for Python and the [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/) HTML and XML document parsing library for Python. 
+
+### Execution Instructions
+Navigate to the directory housing the `course_navigator.py` script. Once your terminal is in the same location as the script (which you can confirm by typing `ls` to view all files in your working directory), use the following command to run the script:
+
+```bash
+python3 course_navigator.py
+```
+> **Note**<br>
+> Your command to initiate Python may vary depending on your computer's configuration. For my computer, the command for running a Python 3 script is `python3` but your computer may use `python`, for example.
+
+### Sample Output
+Once the script executes, you will be greeted with the program's startup message:
+
+```
+--------------------------------------------------------------------------------
+UNIVERSITY OF TORONTO FACULTY OF INFORMATION COURSE NAVIGATOR
+--------------------------------------------------------------------------------
+This tool allows you to browse information on all available iSchool courses.
+
+• Undergraduate courses format: INF000
+• Graduate courses format: INF0000
+
+Please be patient! We fetch data live from the iSchool's website so it may
+take some time to load. We'll let you know when it's ready!
+```
+Seeing this message indicates that the script is running and is in the process of scraping data live from the Faculty of Information's website. Once the scraped data is stored in your Python session, you will be alerted with the following message:
+
+```
+--------------------------------------------------------------------------------
+Courses loaded! HINT: You can leave at any time by typing "exit".
+--------------------------------------------------------------------------------
+To learn about a course, enter a course code: 
+```
+At this point, you may enter in a valid code of a course offered by the Faculty. Courses are coded in the following format: 
+
+* Undergraduate courses are styled `INF000`, with 3 digits. (Ex: `INF452`)
+* Graduate courses are styled `INF0000`, with 4 digits. (Ex: `INF1330`)
+
+Course codes are not case sensitive, but they must match a couse code listed on [this page](https://ischool.utoronto.ca/current-students/programs-courses/courses/course-list).
+
+#### Exiting the program
+Once the data is loaded, you may safely exit the program at any time by typing `exit` instead of a course code. 
+
+## How It Works
+
+The program begins by requesting data from this page of the Faculty's website: [https://ischool.utoronto.ca/current-students/programs-courses/courses/course-list](https://ischool.utoronto.ca/current-students/programs-courses/courses/course-list). It parses the page, turning the text into a Python-readable format and finds each row of the two tables (representing both undergraduate and graduate courses). It then loads the course code, course title, and course link into a Python dictionary.
+
+Next, the program iterates through the new Python dictonary and uses the links found in the previous step to request data from individual course pages from the Faculty's website. It parses the individual course page, loading the course description and any available syllabi into the Python dictionary. 
+
+To find a course, the program matches the user's input against all available course codes listed in the dictionary. If the user's input is found within the dictionary, it returns the collected course information. If the user's input does not match any course within the dictionary, it returns a notification to the user and a prompt to enter a different course code. If the user enters the `exit` command, the program will end and the user will lose all scraped data.
+
+Each time the script runs, the program will fetch data live from the Faculty's website at the aforementioned URL. While this provides the most up-to-date information, if the HTML structure of the page changes this program may not function as expected. 
+
+## Acknowledgments 
+Created by [Sebastian Rodriguez](https://srod.ca), licensed under the [BSD 3-Clause License](https://github.com/seb646/uoft-ischool-course-navigator/blob/main/LICENSE). Built using Python 3, the [Requests](https://requests.readthedocs.io/en/latest/) HTTP client library for Python, and the [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/) HTML and XML document parsing library for Python. 
+
+Prepared for [Dr. Maher Elshakankiri](https://ischool.utoronto.ca/profile/maher-elshakankiri), instructor of [INF452: Information Design Studio V: Coding](https://ischool.utoronto.ca/course/information-design-studio-v-coding) and Assistant Professor, Teaching Stream at the Univeristy of Toronto's Faculty of Information. 
---
+++ course_navigator.py
@@ -0,0 +1,178 @@
+#--------------------------------------------------------------------------------
+# INF452 Midterm
+# University of Toronto, Faculty of Information, Bachelor of Information Program
+#--------------------------------------------------------------------------------
+# Author: Sebastian Rodriguez 
+# Prepared For: Dr. Maher Elshakankiri
+# Date Created: October 18, 2023
+# Last Modified: November 1, 2023
+#--------------------------------------------------------------------------------
+# This program scrapes data on courses within the University of Toronto's
+# Faculty of Information. It captures data directly from the faculty's website,
+# including course title, description, and available syllabi and makes that data
+# available to the user through a command-line interface. Users can browse the
+# faculty's course catalogue using course codes (ex: INF452). 
+#--------------------------------------------------------------------------------
+
+# Import the BeautifulSoup library to handle HTML parsing
+from bs4 import BeautifulSoup
+
+# Import the requests library to handle the HTTP GET requests
+import requests
+
+# Print introduction message to user
+print('-' * 80)
+print("UNIVERSITY OF TORONTO FACULTY OF INFORMATION COURSE NAVIGATOR")
+print('-' * 80)
+print("This tool allows you to browse information on all available iSchool courses.\n")
+print("• Undergraduate courses format: INF000\n• Graduate courses format: INF0000\n")
+print("Please be patient! We fetch data live from the iSchool's website so it may \ntake some time to load. We'll let you know when it's ready!")
+print('-' * 80)
+
+# Define initial search URL
+url = "https://ischool.utoronto.ca/current-students/programs-courses/courses/course-list/"
+
+# Set up HTTP GET request to URL
+page = requests.get(url, timeout=5)
+
+# Use BeautifulSoup to parse the page's content
+content = BeautifulSoup(page.content, 'html.parser')
+
+# Find rows for undergraduate courses
+undergrad_courses_text = content.find("div", id="undergrad_courses") # Find the div
+undergrad_courses_table = undergrad_courses_text.find("table") # Find the table
+undergrad_courses_rows = undergrad_courses_table.find_all("tr") # Find the rows
+
+# Find rows for graduate courses
+grad_courses_text = content.find("div", id="grad_courses") # Find the div
+grad_courses_table = grad_courses_text.find("table") # Find the table
+grad_courses_rows = grad_courses_table.find_all("tr") # Find the rows
+
+# Combine undergraduate and graduate courses
+courses_rows = undergrad_courses_rows + grad_courses_rows
+
+# Set empty dictionary for all courses
+courses = {}
+
+# Iterate through all course rows
+for row in courses_rows:
+
+    # Find all columns in the row
+    course_cols = row.find_all("td")
+
+    # Create empty array for course data
+    course_data = []
+
+    # Iterate through the columns
+    for i, cell in enumerate(course_cols):
+
+        # Add the course title to the course data array
+        course_data.append(cell.text)
+
+        # Check if it's the second column
+        if i == 1:
+
+            # Find the link in the column
+            link = cell.find("a")['href']
+
+            #Append the link to course data
+            course_data.append(link)
+
+    # Check if there's any content in the course data array
+    if course_data:
+        # Remove the H/H1 formatting from the course title
+        title = course_data[0].replace("H1", "")
+        title = title.replace("H", "")
+
+        # Add course data to the global courses dictionary
+        courses[title] = {
+            'title': course_data[1],
+            'link': course_data[2]
+        }
+
+# Iterate through all of the courses found on the previous link
+for code, data in courses.items():
+
+    # Define the individual course URL
+    url = data['link']
+
+    # Establish a new HTTP GET request for course URL
+    page = requests.get(url, timeout=5)
+
+    # Use BeautifulSoup to parse the page's content
+    content = BeautifulSoup(page.content, 'html.parser')
+
+    # Find the course description text
+    course_description = content.find("div", {"class": "post_content"}).find("p")
+
+    # Add course description to the global courses dictionary
+    courses[code]['description'] = course_description.text
+
+    # Find the course syllabi information
+    course_syllabi = content.find("div", {"class": "syllabi"})
+
+    # Check if the course has any available syllabi
+    if course_syllabi is not None:
+
+        # Define an empty dictionary for course syllabi
+        syllabi_store = {}
+
+        # Find all course syllabi links
+        course_syllabi = course_syllabi.find_all("a")
+
+        # Iterate through course syllabi links
+        for syllabus in course_syllabi:
+
+            # Store course syllabi in local dictionary
+            syllabi_store[syllabus.text] = syllabus['href']
+
+        # Add local course syllabi dictionary to global courses dictionary
+        courses[code]['syllabi'] = syllabi_store
+
+# Inform the user that the courses have been loaded
+print("Courses loaded! HINT: You can leave at any time by typing \"exit\".")
+print('-' * 80)
+
+# Ask the user to input a course code
+code = input("To learn about a course, enter a course code: ")
+
+# Loop through while the user has entered a code
+while code is not None:
+
+    # Transform user inputted code to uppercase
+    code = code.upper()
+
+    # If the user says exit, exit the loop
+    if code == "EXIT":
+        print('-' * 80)
+        break
+
+    # Check if the user inputted code is in the global courses dictionary
+    elif code in courses:
+
+        # Print course data to user
+        print('-' * 80)
+        print("Course Code:", code, "\n")
+        print("Course Title:", courses[code]['title'], "\n")
+        print("Course Link:", courses[code]['link'], "\n")
+        print("Course Description:")
+        print(courses[code]['description'])
+
+        # Check if the course has any syllabi
+        if 'syllabi' in courses[code]:
+            print("\nCourse Syllabi:")
+
+            # Iterate through the course syllabi
+            for syllabus in courses[code]['syllabi']:
+                print("    ", syllabus, ":", courses[code]['syllabi'][syllabus])
+
+        # Ask the user to input another course code
+        print('-' * 80)
+        code = input("Enter another course code to continue browsing, or type \"exit\" to leave: ")
+
+    # Run if the user entered a course code that is not found in the global courses dictionary
+    else:
+
+        # Ask the user to input a new course code
+        print('-' * 80)
+        code = input("The course code you entered is not valid. Enter a different code or type \"exit\" to leave: ")
